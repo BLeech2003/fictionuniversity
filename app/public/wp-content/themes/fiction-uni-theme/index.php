1afac1a45645c45eb333fd1a98668990486df253
Bianca Leech <137921358+BLeech2003@users.noreply.github.com>
2024-11-29 10:06:58 +0200

    created base page, update themes

--- app/public/wp-content/themes/fiction-uni-theme/index.php
+++ app/public/wp-content/themes/fiction-uni-theme/index.php
@@ -12,4 +12,16 @@ function varTest($name, $colour){
 
 varTest("Susan", "Red"); -->
 
-<h1><?php bloginfo("name")?></h1>
+<!-- <h1><?php bloginfo("name")?></h1> -->
+
+<?php get_header();
+
+while(have_posts()){
+    the_post();?>
+    <h2> <a href="<?php the_permalink();?>"><?php the_title(); ?> </a>
+    <?php the_content(); ?>
+</hr>
+<?php
+}
+ get_footer();
+ ?>

cc90ee2a8844d04d8c757d6ecc584b19886db92c
Bianca Leech <137921358+BLeech2003@users.noreply.github.com>
2024-12-01 20:41:55 +0200

    some navigation - section 6

--- app/public/wp-content/themes/fiction-uni-theme/header.php
+++ app/public/wp-content/themes/fiction-uni-theme/header.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes() ?>>
 
 <head>
+    <meta charset="<?php bloginfo("charset") ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class() ?>>
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
-                <a href="<?php echo site_url()?>"><strong>Fictional</strong> University</a>
+                <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
             </h1>
             <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
                     aria-hidden="true"></i></span>
@@ -17,11 +19,11 @@
             <div class="site-header__menu group">
                 <nav class="main-navigation">
                     <ul>
-                        <li><a href="<?php echo site_url("/about-us")?>">About Us</a></li>
-                        <li><a href="<?php echo site_url("/about-us")?>">Programs</a></li>
-                        <li><a href="<?php echo site_url("/about-us")?>">Events</a></li>
-                        <li><a href="<?php echo site_url("/about-us")?>">Campuses</a></li>
-                        <li><a href="<?php echo site_url("/about-us")?>">Blog</a></li>
+                        <li><a href="<?php echo site_url("/about-us") ?>">About Us</a></li>
+                        <li><a href="<?php echo site_url("/about-us") ?>">Programs</a></li>
+                        <li><a href="<?php echo site_url("/about-us") ?>">Events</a></li>
+                        <li><a href="<?php echo site_url("/about-us") ?>">Campuses</a></li>
+                        <li><a href="<?php echo site_url("/about-us") ?>">Blog</a></li>
                     </ul>
                 </nav>
                 <div class="site-header__util">
